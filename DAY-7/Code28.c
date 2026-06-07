8a75ca8b063205bc73be6d4e69870b2fc44808e8
Dhriti-Singh-Sisodia <thisisdhritisingh@gmail.com>
2026-06-07 21:35:04 +0530

    DAY-7 assignment is uploaded

---
+++ DAY-7/Code28.c
@@ -0,0 +1,20 @@
+//WAP to recursive reverse number//
+#include<stdio.h>
+int reverseNumber(int n, int rev)
+{
+    if (n == 0)
+    {
+        return rev;
+    }
+    rev = rev * 10 + n % 10;
+    return reverseNumber(n / 10, rev);
+}
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int reversed = reverseNumber(num, 0);
+    printf("Reversed number of %d is %d\n", num, reversed);
+    return 0;
+}
