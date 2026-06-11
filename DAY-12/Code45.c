2904194f5be79e7b0e0f084c62572f1b6b94e88d
Dhriti-Singh-Sisodia <thisisdhritisingh@gmail.com>
2026-06-11 09:25:22 +0530

    DAY-12 assignment is uploaded

---
+++ DAY-12/Code45.c
@@ -0,0 +1,25 @@
+//WAP to write function for palindrome//
+#include<stdio.h>
+int palindrome(int n)
+{
+    int rev=0, rem, a = n;
+    while(a>0)
+    {
+        rem = a%10;
+        rev = rev*10 + rem;
+        a = a/10;
+    }
+    return (rev == n);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(palindrome(num))
+        printf("%d is a palindrome number.\n", num);
+    else
+        printf("%d is not a palindrome number.\n", num);
+    return 0;
+}
