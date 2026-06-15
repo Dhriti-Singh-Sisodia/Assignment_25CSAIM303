45b882d7ad2b919b6a78125885e008956b9c80fd
Dhriti-Singh-Sisodia <thisisdhritisingh@gmail.com>
2026-06-15 19:55:42 +0530

    DAY-15 assignment is uploaded

---
+++ DAY-15/Code59.c
@@ -0,0 +1,25 @@
+//WAP to rotate array right//
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,temp;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    printf("Enter the elements of array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    temp=a[n-1];
+    for(i=n-1;i>0;i--)
+    {
+        a[i]=a[i-1];
+    }
+    a[0]=temp;
+    printf("The right rotated array is:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
