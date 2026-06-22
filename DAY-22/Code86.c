f361cfea96011ff1139e955312092932d3b8ddf8
Dhriti-Singh-Sisodia <thisisdhritisingh@gmail.com>
2026-06-22 16:51:55 +0530

    DAY-22 assignment is uploaded

---
+++ DAY-22/Code86.c
@@ -0,0 +1,23 @@
+//WAP to count words in a sentence//
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, words = 1;
+
+    printf("Enter sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ' && str[i + 1] != ' ')
+        {
+            words++;
+        }
+    }
+
+    printf("Number of words = %d", words);
+
+    return 0;
+}
+
